6bf0dc69dd858b2f8ef6c09c8227a428509f2e13
preshia <preshia@naver.com>
2017-04-06 11:43:17 +0900

    미니맵 완성했습니다

---
+++ School Student Shot_1/Assets/Scripts/CJoyPad.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CJoyPad : MonoBehaviour {
+
+	public CPlayer mpPlayerScript;
+
+
+	public RectTransform mpJoyPadBackRect;
+	public Transform mpStickTr;
+	private Vector3 mDefaultPos;
+	private Vector3 mScreenPos;
+	private Vector3 mTPos;
+	private Vector3 mMove;
+	private Touch mTouch;
+	private float mRadian;
+
+	// Use this for initialization
+	void Start () {
+		mRadian = mpJoyPadBackRect.sizeDelta.y / 2;
+		mDefaultPos = mpStickTr.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DoJoyPadMove()
+	{
+		mTouch = Input.GetTouch (Input.touchCount -1);
+		mTPos = new Vector3 (mTouch.position.x, mTouch.position.y, mpStickTr.position.z);
+		mMove = (mTPos - mDefaultPos).normalized;
+		float tDistance = Vector3.Distance (mTPos, mDefaultPos);
+
+		if (tDistance > mRadian) {
+			mScreenPos = mDefaultPos + mMove * mRadian;
+		} else {
+			mScreenPos = mDefaultPos + mMove * tDistance;
+		}
+
+		float tRotationValue = (Mathf.Atan2 (mMove.x, mMove.y) * 180.0f) / Mathf.PI;
+
+		if (tRotationValue < 0) {
+			tRotationValue += 360.0f;
+		}
+
+		mpPlayerScript.SetMoveInput (mMove);
+		mpPlayerScript.SetRotateInput (tRotationValue);
+		mpPlayerScript.setIsJoyPadTouched (true);
+		mpStickTr.position = mScreenPos;
+
+
+	}
+
+	public void DoJoyPadEnd()
+	{
+		mpPlayerScript.setIsJoyPadTouched (false);
+		mpStickTr.position = mDefaultPos;
+	}
+
+
+}
---
+++ School Student Shot_1/Assets/Scripts/CPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPlayer : MonoBehaviour {
+
+	public bool mIsJoyPadTouched = false;
+	public Vector3 mMoveVector = Vector3.zero;
+	public float mSpeed = 2.0f;
+	//public Rigidbody mRigidbody;
+	public CharacterController mController = null;
+	private float mRotationValue = 0.0f;
+
+	Vector3 mTempPosition = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		//mRigidbody = this.gameObject.GetComponent<Rigidbody> ();
+		mController = this.gameObject.GetComponent<CharacterController> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(true == mIsJoyPadTouched)
+		{
+			//transform.localPosition += mMoveVector * Time.deltaTime * mSpeed;
+			transform.rotation = Quaternion.Euler (new Vector3(0,mRotationValue, 0));
+			//transform.rotation = Quaternion.Euler();
+			//mRigidbody.MovePosition(transform.position + mMoveVector * Time.deltaTime*mSpeed);
+			mController.Move( mMoveVector * Time.deltaTime*mSpeed);
+		}
+	}
+
+	public void SetMoveInput(Vector3 tTemp)
+	{
+		mMoveVector = new Vector3(tTemp.x, 0, tTemp.y);
+	}
+
+	public void SetRotateInput(float tTemp)
+	{
+		mRotationValue = tTemp;
+	}
+
+	public void setIsJoyPadTouched(bool tTemp)
+	{
+		mIsJoyPadTouched = tTemp;
+	}
+		
+
+
+}
